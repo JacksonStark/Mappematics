6dfb2639403f655333bc9fb4f2834c26996df441
Jackson Stark <jacksonstark77@hotmail.com>
2019-12-23 00:27:36 -0600

    initialized git repo and map template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker }  from 'react-google-maps';
+
+
+
+const MapComponent = withScriptjs(withGoogleMap((props) => 
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    >
+      {props.marker && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+    </GoogleMap>
+))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <MapComponent 
+        marker
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyB3QzhoUs4GVR_lGdpciw92E4VDFYE3EVc&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `800px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+      <h1 style={{ position: `absolute`, right: `8%`, bottom: `8%`, color: `red`, fontStyle: `italic`, fontSize: 88 }}>Mappematics</h1>
+    </>
+  )
 }
 
 export default App;
+
+
+// API KEY:
+// ^v^
+// AIzaSyB3QzhoUs4GVR_lGdpciw92E4VDFYE3EVc
